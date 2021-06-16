8c651fdbf58cff22a963f27a97c75eb82a430a27
irasan <iryna.sanzhara@gmail.com>
2021-06-16 21:54:06 +0000

    Add registration and login functionality

--- app.py
+++ app.py
@@ -24,6 +24,90 @@ def home():
     return render_template("home.html", categories=categories)
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        if (request.form.get("password") == request.form.get("password1")):
+            register = {
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(
+                    request.form.get("password")),
+                "is_admin": False,
+                "reviews": [],
+                "favorites": [],
+                "avatar": request.form.get("avatar")
+            }
+            mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}!".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    if "user" in session:
+        if session["user"] == username:
+            favs_list = mongo.db.users.find_one(
+                {"username": username})["favorites"]
+            favs = mongo.db.art.find(
+                {"_id": {"$in": favs_list}}).sort([("title", 1)])
+            print(favs)
+            
+            return render_template(
+                "profile.html", favs=favs)
+        return redirect(url_for("home"))
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
